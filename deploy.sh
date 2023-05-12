866ed1565dff805de5ae580450dde49dcece9bfc
Cymekk <cymerman944@gmail.com>
2023-05-12 09:37:39 +0200

    deploy.sh has been updated

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Cymekk/wallet.git master:gh-pages
+git push -f git@github.com:Cymekk/kalkulator-kalorii.git master:gh-pages
 
 cd -
